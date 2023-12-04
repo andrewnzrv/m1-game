722175ddd37071f995158fa79e6a59727f45bfc3
Andrei Nazarov <andrei.nazarov@tum.de>
2023-12-04 14:10:41 +0100

    Added collision

--- js/game.js
+++ js/game.js
@@ -3,9 +3,10 @@ class Game {
     this.gameStartScreen = document.getElementById("game-start-screen");
     this.gameScreen = document.getElementById("game-screen");
     this.gameEndScreen = document.getElementById("game-end-screen");
-
     this.player = new Player(0, 0, 200, 100, "./images/player.png");
     this.obstacles = [];
+    this.lives = 3;
+    this.score = 0;
   }
 
   start() {
@@ -23,16 +24,26 @@ class Game {
   update() {
     this.player.move();
 
-    this.obstacles.forEach((obstacle) => {
+    for (let i = 0; i < this.obstacles.length; i++) {
+      const obstacle = this.obstacles[i];
       obstacle.move();
-    });
 
-    this.obstacles.forEach((obstacle) => {
+      // Remove obstacles that are off the screen from the DOM and array
       if (obstacle.left + obstacle.width < 0) {
         obstacle.element.remove();
-        this.obstacles.splice(obstacle, 1);
+        this.obstacles.splice(i, 1);
+        i--;
       }
-    });
+
+      // Check for collision
+      else if (this.player.didCollide(obstacle)) {
+        obstacle.element.remove();
+        this.obstacles.splice(i, 1);
+        this.lives--;
+        console.log(this.lives);
+        i--;
+      }
+    }
 
     // Set the frequency of adding new obstacles
     if (Math.random() > 0.99) {
